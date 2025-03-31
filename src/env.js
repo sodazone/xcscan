1ad8682b4c501b09e21c62f8503d9ce1f3c4a755
Marc Fornos <marc.fornos@gmail.com>
2025-03-31 12:40:59 +0200

    adjust env

--- src/env.js
+++ src/env.js
@@ -1,2 +1,2 @@
-export const httpUrl = import.meta.env.VITE_API_HTTP_URL;
-export const apiKey = import.meta.env.VITE_API_API_KEY;
+export const httpUrl = import.meta.env.VITE_OC_API_HTTP_URL;
+export const apiKey = import.meta.env.VITE_OC_API_KEY;
